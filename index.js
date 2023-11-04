b9efbd789e66904f5b1b9e877f2b68026867b57c
Ianc Samac <ianksamacs@gmail.com>
2023-11-04 00:08:25 -0300

    Instalação Node

---
+++ index.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+
+function getProdutos(){
+  const webhookURL = 'http://187.111.192.23:5678/webhook/pdv/produtos';
+  
+  // Configuração da requisição
+  const requestOptions = {
+    method: 'GET', // Você pode usar 'GET' ou 'POST' dependendo da sua webhook
+    url: webhookURL,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+
+  // Chama a webhook usando fetch
+  axios(requestOptions)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.error('Erro na requisição:', error);
+    });
+}
+
+function deleteProdutos(id){
+  const webhookURL = 'http://187.111.192.23:5678/webhook-test/pdv/produtos';
+  
+  const data = {
+    id: id
+  };
+
+  // Configuração da requisição
+  const requestOptions = {
+    method: 'DELETE', // Você pode usar 'GET' ou 'POST' dependendo da sua webhook
+    url: webhookURL,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    params: JSON.stringify(data)
+  };
+
+  // Chama a webhook usando fetch
+  axios(requestOptions)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.error('Erro na requisição:', error);
+    });
+}
+
+deleteProdutos();
